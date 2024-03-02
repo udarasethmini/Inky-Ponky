216eab6d2720ad204b82d517a3409a2944e82de2
udarasethmini <161945789+udarasethmini@users.noreply.github.com>
2024-03-02 18:34:45 +0530

    Create Inky_ponkycode.java

---
+++ Inky_ponkycode.java
@@ -0,0 +1,19 @@
+public class Inky{
+  public Inky(String pinky){
+    System.out.println(pinky);
+  }
+  public Inky(String pinky,String blinky,String rinky){
+    System.out.println(blinky +"" +pinky);
+    System.out.println(pinky +"" +rinky);
+    System.out.println(blinky +"" +pinky +"" +rinky);
+  }
+  public static void main(String args[]){
+    String pinky = "Donky";
+    String blinky = "Pinky";
+    String rinky ="Monkey";
+
+  Inky ponky = new Inky(pinky);
+    Inky ponky2=new
+      Inky(pinky,blinky,rinky);
+  }
+}
